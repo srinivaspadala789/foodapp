22fb2ff01302ba66df36438046dc422bba8f0da5
srinivaspadala789 <srinivaspadala789@gmail.com>
2025-12-09 22:06:18 +0530

    Setup gh-pages deployment

---
+++ food-app/src/App.js
@@ -0,0 +1,22 @@
+import logo from './logo.svg';
+import './App.css';
+
+
+const Header = () =>{
+  return(
+    <div className = 'Header'>
+    <h1>header</h1>
+    </div>
+  )
+}
+
+function App() {
+  return (
+    <div>
+      <Header/>
+     <h1>djddj</h1>
+    </div>
+  );
+}
+
+export default App;
